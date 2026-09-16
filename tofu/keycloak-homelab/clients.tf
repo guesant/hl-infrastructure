9f81e9c1ebb445e837e649862d3ca101a99fe078
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-09-16 15:33:35 -0300

    chore(tofu): drop the import blocks and the moved homelab clients

--- tofu/keycloak-homelab/clients.tf
+++ tofu/keycloak-homelab/clients.tf
@@ -7,21 +7,5 @@ locals {
       logout_redirect_uri = "https://${var.blog_hostname}/signout-callback-oidc"
       secret              = var.blog_client_secret
     }
-    argocd = {
-      name                = "Argo CD (moved to the management realm)"
-      enabled             = false
-      base_url            = "https://argocd.${var.internal_domain}"
-      redirect_uris       = ["https://argocd.${var.internal_domain}/auth/callback"]
-      logout_redirect_uri = "https://argocd.${var.internal_domain}/*"
-      secret              = "disabled-client-moved-to-management-realm"
-    }
-    grafana = {
-      name                = "Grafana (moved to the management realm)"
-      enabled             = false
-      base_url            = "https://grafana.${var.internal_domain}"
-      redirect_uris       = ["https://grafana.${var.internal_domain}/login/generic_oauth"]
-      logout_redirect_uri = "https://grafana.${var.internal_domain}/*"
-      secret              = "disabled-client-moved-to-management-realm"
-    }
   }
 }
