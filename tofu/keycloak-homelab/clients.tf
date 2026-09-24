908645152880b0f353a654ee7c431bb4ab0ec247
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2026-09-24 12:11:24 -0300

    fix(keycloak): align blog client callbacks

--- tofu/keycloak-homelab/clients.tf
+++ tofu/keycloak-homelab/clients.tf
@@ -1,10 +1,12 @@
 locals {
+  admin_base_url = "https://admin.${var.blog_hostname}"
+
   clients = {
     blog = {
-      name                = var.blog_hostname
-      base_url            = "https://${var.blog_hostname}"
-      redirect_uris       = ["https://${var.blog_hostname}/signin-oidc"]
-      logout_redirect_uri = "https://${var.blog_hostname}/signout-callback-oidc"
+      name                = "${var.blog_hostname} admin"
+      base_url            = local.admin_base_url
+      redirect_uris       = ["${local.admin_base_url}/auth/keycloak/callback"]
+      logout_redirect_uri = "${local.admin_base_url}/"
       secret              = var.blog_client_secret
     }
   }
